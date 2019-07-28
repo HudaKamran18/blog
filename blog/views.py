2df6bef533047021a01665e1753f61b2f8f7f55a
HudaKamran18 <h.kam.9213@gmail.com>
2019-07-28 17:12:28 +0500

    Blog app, first commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+
+# def about(request):
+#     return render(request, 'blog/about.html')
+
+
+# def services(request):
+#     return render(request, 'blog/services.html')
