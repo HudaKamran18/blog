cffeec6f1087fc9d50239390a18c6a8fda9ff83d
HudaKamran18 <h.kam.9213@gmail.com>
2019-07-29 14:39:48 +0500

    added page templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    # path('about/', views.about, name='about'),
-    # path('services/', views.services, name='services'),
-    # path('works/', views.works, name='works'),
-    # path('blog', views.blog, name='blog'),
-    # path('contact', views.contact, name='contact')
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('works/', views.works, name='works'),
+    path('blog', views.blog, name='blog'),
+    path('contact', views.contact, name='contact')
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,21 @@ def index(request):
     return render(request, 'blog/index.html')
 
 
-# def about(request):
-#     return render(request, 'blog/about.html')
+def about(request):
+    return render(request, 'blog/about.html')
 
 
-# def services(request):
-#     return render(request, 'blog/services.html')
+def services(request):
+    return render(request, 'blog/services.html')
+
+
+def works(request):
+    return render(request, 'blog/works.html')
+
+
+def blog(request):
+    return render(request, 'blog/blog.html')
+
+
+def contact(request):
+    return render(request, 'blog/contact.html')
